cc8349787685efe5f77e351b669270d841ac7792
Evelynpa <evelyn.parra.rojas@gmail.com>
2018-01-08 17:34:50 -0300

    Etapa 9 se agrega clase active a div

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -8,8 +8,12 @@ $(document).ready(function(){
 
 $('.js-show-recipe').on('click', function(){
 	$('.page').removeClass('make');
+	$('.js-show-recipe').addClass('active');
+    $('.js-show-make').removeClass('active');
 });
 
 $('.js-show-make').on('click', function(){
 	$('.page').addClass('make');
+	$('.js-show-make').addClass('active');
+    $('.js-show-recipe').removeClass('active');
 });
